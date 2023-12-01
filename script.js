f4c3cb577fe27ebfb791cca0f04b26ecfec63e2c
ViniCavalAraujo <149206889+ViniCavalAraujo@users.noreply.github.com>
2023-11-30 21:13:59 -0300

    1º site usando formulário

---
+++ script.js
@@ -0,0 +1,36 @@
+function validacao(){
+
+    var nome = document.getElementById("nome").value;
+    var tipoDoContato = document.getElementById("tipoContato").value;
+    var telefone = document.getElementById("telefone").value;
+    var generoMasc = document.getElementById("generoMasc").checked;
+    var generoFem = document.getElementById("generoFem").checked;
+    var generoOutro = document.getElementById("generoOutro").checked;
+    var mensagem = document.getElementById("mensagem").value;
+
+
+
+    return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //GIT
+    //DEPLOY
+    //DNS REGISTRO BR
+    //SQL PHP 
